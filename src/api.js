9a99e3dd1d3024a9fbb5689062647da65fc74c7d
Wanjiru Kariuki <wanjiruregina678@gmail.com>
2025-06-20 14:24:18 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // Update if needed
+const BASE_URL = 'http://localhost:5000'; 
 
 export const getHabits = () => axios.get(`${BASE_URL}/habits`);
 
 export const joinHabit = (habitId) =>
   axios.post(`${BASE_URL}/habits/${habitId}/join`);
+
+export const getChallengeParticipants = (challengeId) =>
+  axios.get(`http://localhost:5000/challenges/${challengeId}/participants`);
+
+export const addParticipantToChallenge = (challengeId, data) =>
+  axios.post(`http://localhost:5000/challenges/${challengeId}/participants`, data);
