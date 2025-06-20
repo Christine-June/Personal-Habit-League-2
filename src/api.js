d1d6b0c930270042ff4e11a8f789da804bc8151e
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-06-20 14:17:46 +0300

    add functions to get and add challenge participants

--- src/api.js
+++ src/api.js
@@ -6,3 +6,10 @@ export const getHabits = () => axios.get(`${BASE_URL}/habits`);
 
 export const joinHabit = (habitId) =>
   axios.post(`${BASE_URL}/habits/${habitId}/join`);
+
+export const getChallengeParticipants = (challengeId) =>
+  axios.get(`http://localhost:5000/challenges/${challengeId}/participants`);
+
+export const addParticipantToChallenge = (challengeId, data) =>
+  axios.post(`http://localhost:5000/challenges/${challengeId}/participants`, data);
+
