ca301307b6f05a2e25988f4bfdf7097091e1c55a
Brian Kaloki <brianngenye@gmail.com>
2025-06-23 23:54:18 +0300

    cleamimh app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,46 @@
+// src/App.jsx
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+
 import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
 
 import HomePage from './pages/HomePage';
 import UsersPage from './pages/UsersPage';
 import HabitsPage from './pages/HabitsPage';
-import SideBar from './components/SideBar';
 import ChallengesPage from './pages/ChallengesPage';
 import UserHabitsPage from './pages/UserHabitsPage';
 import ChallengeParticipantsPage from './pages/ChallengeParticipantsPage';
 import ChallengeEntriesPage from './pages/ChallengeEntriesPage';
 import HabitEntriesPage from './pages/HabitEntriesPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import CookieSettings from './pages/CookieSettings';
 import LoginSignupPage from './pages/LoginSignupPage';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  useLocation,
-} from 'react-router-dom';
+import SideBar from './components/SideBar';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function AppContent() {
   const [sidebarExpanded, setSidebarExpanded] = useState(true);
   const location = useLocation();
 
-  const isAuthPage = location.pathname === '/auth';
+  // Pages that don't require sidebar/navbar/footer
+  const isMinimalPage = ["/auth", "/privacy", "/terms", "/cookies"].includes(location.pathname);
 
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      {!isAuthPage && (
+      {!isMinimalPage && (
         <SideBar expanded={sidebarExpanded} setExpanded={setSidebarExpanded} />
       )}
       <div className="flex-1 flex flex-col">
-        {!isAuthPage && (
-          <Navbar
-            sidebarExpanded={sidebarExpanded}
-            setSidebarExpanded={setSidebarExpanded}
-          />
+        {!isMinimalPage && (
+          <Navbar sidebarExpanded={sidebarExpanded} setSidebarExpanded={setSidebarExpanded} />
         )}
-        <div className={`flex-grow ${!isAuthPage ? 'pt-16 pb-24' : ''}`}>
+        <div className={`flex-grow ${!isMinimalPage ? 'pt-16 pb-24' : ''}`}>
           <Routes>
-            {/* 🔁 Redirect root to /auth */}
+            {/* Redirect root to /auth */}
             <Route path="/" element={<Navigate to="/auth" />} />
             <Route path="/auth" element={<LoginSignupPage />} />
             <Route path="/home" element={<HomePage sidebarExpanded={sidebarExpanded} />} />
@@ -54,9 +51,12 @@ function AppContent() {
             <Route path="/challenge-participants" element={<ChallengeParticipantsPage />} />
             <Route path="/challenge-entries" element={<ChallengeEntriesPage />} />
             <Route path="/habit-entries" element={<HabitEntriesPage />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/cookies" element={<CookieSettings />} />
           </Routes>
         </div>
-        {!isAuthPage && <Footer sidebarExpanded={sidebarExpanded} />}
+        {!isMinimalPage && <Footer sidebarExpanded={sidebarExpanded} />}
       </div>
     </div>
   );
@@ -67,64 +67,6 @@ function App() {
     <ThemeProvider>
       <Router>
         <AppContent />
-      </Router>
-    </ThemeProvider>
-  );
-}
-
-export default App;
-
-// src/App.jsx
-import { useState } from 'react';
-import { ThemeProvider } from './context/ThemeContext';
-import DarkModeToggle from './components/DarkModeToggle';
-import './App.css';
-
-import HomePage from './pages/HomePage';
-import UsersPage from './pages/UsersPage';
-import HabitsPage from './pages/HabitsPage';
-import SideBar from './components/SideBar';
-import ChallengesPage from './pages/ChallengesPage';
-import UserHabitsPage from './pages/UserHabitsPage';
-import ChallengeParticipantsPage from './pages/ChallengeParticipantsPage';
-import ChallengeEntriesPage from './pages/ChallengeEntriesPage';
-import HabitEntriesPage from './pages/HabitEntriesPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfService from './pages/TermsOfService';
-import CookieSettings from './pages/CookieSettings'; // Import the new CookieSettings component
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  const [sidebarExpanded, setSidebarExpanded] = useState(true);
-
-  return (
-    <ThemeProvider>
-      <Router>
-        <div className="min-h-screen flex flex-col md:flex-row">
-          <SideBar expanded={sidebarExpanded} setExpanded={setSidebarExpanded} />
-          <div className="flex-1 flex flex-col">
-            <Navbar sidebarExpanded={sidebarExpanded} setSidebarExpanded={setSidebarExpanded} />
-            <div className="flex-grow pt-16 pb-24"> {/* Add top and bottom padding to prevent overlap */}
-              <Routes>
-                <Route path="/" element={<HomePage sidebarExpanded={sidebarExpanded} />} />
-                <Route path="/users" element={<UsersPage />} />
-                <Route path="/habits" element={<HabitsPage />} />
-                <Route path="/challenges" element={<ChallengesPage />} />
-                <Route path="/user-habits" element={<UserHabitsPage />} />
-                <Route path="/challenge-participants" element={<ChallengeParticipantsPage />} />
-                <Route path="/challenge-entries" element={<ChallengeEntriesPage />} />
-                <Route path="/habit-entries" element={<HabitEntriesPage />} />
-                <Route path="/privacy" element={<PrivacyPolicy />} />
-                <Route path="/terms" element={<TermsOfService />} />
-                <Route path="/cookies" element={<CookieSettings />} /> {/* Add the new route here */}
-              </Routes>
-            </div>
-            <Footer sidebarExpanded={sidebarExpanded} />
-          </div>
-        </div>
       </Router>
     </ThemeProvider>
   );
