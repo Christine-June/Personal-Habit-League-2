961f0f40ee8e60b700ef88d9aea911e142a3dbb8
Priscillah Njai <priscillahnjaii@gmail.com>
2025-06-28 15:13:21 +0300

    Complete app with privacy policy, terms of service, cookie settings, chat log, and blog features

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,27 @@ import ToastConfig from './components/ToastConfig';
 import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    const savedUser = localStorage.getItem('currentUser');
-    if (savedUser) {
-      setCurrentUser(JSON.parse(savedUser));
-    }
-  }, []);
-
-  return (
-    <ThemeProvider>
-      <Router>
-       <Toaster position="top-right" reverseOrder={false} />
-        <ToastConfig />
-      </Router>
-    </ThemeProvider>
-  );
-}
-
-export default App;
 import HomePage from './pages/HomePage';
 import UsersPage from './pages/UsersPage';
 import HabitsPage from './pages/HabitsPage';
 import ChallengesPage from './pages/ChallengesPage';
+import UserHabitsPage from './pages/UserHabitsPage';
+import ChallengeParticipantsPage from './pages/ChallengeParticipantsPage';
+import ChallengeEntriesPage from './pages/ChallengeEntriesPage';
+import HabitEntriesPage from './pages/HabitEntriesPage';
+import ChallengeDetailPage from './pages/ChallengeDetailPage'; // New Import
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import CookieSettings from './pages/CookieSettings';
 import LoginSignupPage from './pages/LoginSignupPage';
+import ChatLog from "./components/ChatLog";
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogPage from './pages/BlogPage';
 
 import SideBar from './components/SideBar';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 function AppContent({ currentUser, setCurrentUser }) {
   const [sidebarExpanded, setSidebarExpanded] = useState(true);
   const location = useLocation();
@@ -57,20 +47,50 @@ function AppContent({ currentUser, setCurrentUser }) {
           <Navbar sidebarExpanded={sidebarExpanded} setSidebarExpanded={setSidebarExpanded} />
         )}
         <div className={`flex-grow ${!isMinimalPage ? 'pt-16 pb-24' : ''}`}>
-          {/* Routes will be added here */}
-        </div>
-        {!isMinimalPage && <Footer sidebarExpanded={sidebarExpanded} />}
-      </div>
-    </div>
-  )
-}
-<Routes>
+          <Routes>
             <Route path="/" element={<Navigate to="/auth" />} />
             <Route path="/auth" element={<LoginSignupPage setCurrentUser={setCurrentUser} />} />
             <Route path="/home" element={<HomePage sidebarExpanded={sidebarExpanded} />} />
             <Route path="/users" element={<UsersPage />} />
             <Route path="/habits" element={<ProtectedRoute><HabitsPage /></ProtectedRoute>} />
             <Route path="/challenges" element={<ProtectedRoute><ChallengesPage /></ProtectedRoute>} />
+            <Route path="/user-habits" element={<UserHabitsPage />} />
+            <Route path="/challenges/:challengeId" element={<ProtectedRoute><ChallengeDetailPage /></ProtectedRoute>} /> {/* New Route */}
+            <Route path="/challenge-participants" element={<ChallengeParticipantsPage />} />
+            <Route path="/challenge-entries" element={<ChallengeEntriesPage />} />
+            <Route path="/habit-entries" element={<ProtectedRoute><HabitEntriesPage /></ProtectedRoute>} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/cookies" element={<CookieSettings />} />
+            <Route path="/chat" element={<ChatLog currentUser={currentUser} />} />
+            <Route path="/blog" element={<BlogPage />} />
           </Routes>
+        </div>
+        {!isMinimalPage && <Footer sidebarExpanded={sidebarExpanded} />}
+      </div>
+    </div>
+  )
+}
 
-          
+function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('currentUser');
+    if (savedUser) {
+      setCurrentUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <Router>
+       <Toaster position="top-right" reverseOrder={false} />
+        <ToastConfig />
+        <AppContent currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
