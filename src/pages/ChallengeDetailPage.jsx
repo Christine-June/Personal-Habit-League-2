1adad7c825c89ebad143363ea17983eda314d63f
Priscillah Njai <priscillahnjaii@gmail.com>
2025-06-28 14:50:05 +0300

    Adds responsive padding and typography styling for better visual presentation

---
+++ src/pages/ChallengeDetailPage.jsx
@@ -0,0 +1,10 @@
+// src/pages/ChallengeDetailPage.jsx
+import React from 'react';
+
+export default function ChallengeDetailPage() {
+  return (
+    <div>
+      <h1>Challenge Details</h1>
+    </div>
+  );
+}
