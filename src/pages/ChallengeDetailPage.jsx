7cc58a73d649615467b9925e25ccb970da3549e8
Priscillah Njai <priscillahnjaii@gmail.com>
2025-06-28 16:20:23 +0300

    Adds challenge page paragraphs

--- src/pages/ChallengeDetailPage.jsx
+++ src/pages/ChallengeDetailPage.jsx
@@ -5,7 +5,7 @@ export default function ChallengeDetailPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Challenge Details</h1>
-      <p>This page will show challenge details, participants, and entries.</p>
+      <p>Challenge details, participants, and entries.</p>
     </div>
   );
 }
